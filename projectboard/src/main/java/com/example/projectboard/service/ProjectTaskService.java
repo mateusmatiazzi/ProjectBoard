5ec6931e8518a0e1562f4eb6e8d62f84091ea268
mateus <mateuspaivamatiazzi1@gmail.com>
2020-05-09 22:37:49 -0400

    Adição da classe base.

---
+++ projectboard/src/main/java/com/example/projectboard/service/ProjectTaskService.java
@@ -0,0 +1,12 @@
+package com.example.projectboard.service;
+
+import com.example.projectboard.repository.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+}
