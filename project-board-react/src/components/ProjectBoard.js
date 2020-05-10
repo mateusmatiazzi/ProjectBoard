d91e2ed152de98bd5daea56beeb8802a504c86e7
mateus <mateuspaivamatiazzi1@gmail.com>
2020-05-10 18:40:33 -0400

    Mudança de estado para as tarefas

--- project-board-react/src/components/ProjectBoard.js
+++ project-board-react/src/components/ProjectBoard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {Link} from "react-router-dom";
+import ProjectTaskItem from './ProjectTask/ProjectTaskItem';
 
 class ProjectBoard extends Component {
     render() {
@@ -22,26 +23,7 @@ class ProjectBoard extends Component {
                         {
                             //<!-- SAMPLE PROJECT TASK STARTS HERE -->
                         }
-                        <div className="card mb-1 bg-light">
-    
-                            <div className="card-header text-primary">
-                                ID: projectSequence
-                            </div>
-                            <div className="card-body bg-light">
-                                <h5 className="card-title">summary</h5>
-                                <p className="card-text text-truncate ">
-                                    acceptanceCriteria
-                                </p>
-                                <a href="/ProjectTaskForm.html" className="btn btn-primary">
-                                    View / Update
-                                </a>
-    
-                                <button className="btn btn-danger ml-4">
-                                    Delete
-                                </button>
-                            </div>
-                        </div>
-    
+                        <ProjectTaskItem/>
                         {
                             //<!-- SAMPLE PROJECT TASK ENDS HERE -->
                         }
@@ -55,7 +37,7 @@ class ProjectBoard extends Component {
                         {
                             //<!-- SAMPLE PROJECT TASK STARTS HERE -->
                         }
-
+                        <ProjectTaskItem/>
                         {
                             //<!-- SAMPLE PROJECT TASK ENDS HERE -->
                         }
